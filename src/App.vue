97b5e8e2fff195cfd609074ac65e7f2d344aad55
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-06-28 20:40:50 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+ 
+   <WeatherInfo/>
+
+  </div>
+  
+</template>
+
+<script>
+import WeatherInfo from './components/WeatherInfo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeatherInfo
+  }
+}
+</script>
+
+<style>
+
+div{
+  
+
+    justify-content: center;
+
+}
+
+  
+
+</style>
